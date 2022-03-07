27cd964df6e149a45d469daecd837628bb996c2c
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-07 20:35:19 +0530

    [Guru] Create DataTable -UC1

---
+++ AdvAddressBookLINQ/AddressBookManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvAddressBookLINQ
+{
+    public class AddressBookManager
+    {
+        public static DataTable dataTable;
+    }
+}
