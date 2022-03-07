5347749b74f30ec9b4733d7a29ead31a1563de32
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-07 19:52:39 +0530

    [Guru] Welcome to advanced AddressBook using LINQ

---
+++ AdvAddressBookLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvAddressBookLINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Advanced AddressBook system using LINQ");
+            Console.ReadLine();
+        }
+    }
+}
